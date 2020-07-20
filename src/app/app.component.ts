17049b98e14e5e6e73e202c28f25b0d905505af4
Sparsh Goel <sparsh.goel.sd@mmumullana.org>
2020-07-20 23:36:36 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  componentTitle = 'I am title from component.ts';
+  clickHandler(){
+    alert("Hello sir");
+  }
 }
